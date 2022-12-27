95e63719f5b2126aef855f6d40e1f1cd7b682c59
Tomas Vazquez <vazquezt2018@gmail.com>
2022-12-27 16:54:16 -0300

    fix: updated the font size of titles

--- src/components/TitleRemarcado.js
+++ src/components/TitleRemarcado.js
@@ -4,7 +4,7 @@ import './Titles.css'
 
 const TitleRemarcado = (props) => {
   return (
-    <Text fontSize={["30px","40px","40px"]} id = 'titles'>- {props.contenido} -
+    <Text fontSize={["25px","40px","40px"]} id = 'titles'>- {props.contenido} -
   </Text>
   )
 }

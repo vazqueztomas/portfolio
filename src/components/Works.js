b4dad3d0a5eb2f0f16532f8db84617811e9d4e9e
Tomas Vazquez <vazquezt2018@gmail.com>
2023-03-15 18:44:02 -0300

    finally updated projects section

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,45 +1,24 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react';
 import TitleRemarcado from './TitleRemarcado';
-import codewitchers from '../assets/images/codewitchers.svg';
-import cancheros from '../assets/images/cancheros.svg';
-import devaluarg from '../assets/images/devaluarg.svg';
 import './Works.css';
+import { works } from './worksArray';
 
 export default function App() {
   const [showCard, setShowCard] = useState(false);
   const [selectedWorkIndex, setSelectedWorkIndex] = useState(-1);
 
-  const works = [
-    {
-      title: 'OnEmpleo',
-      src: 'https://onempleo.com/assets/logo_OnEmpleo.565f4192.svg',
-      description:
-        'OnEmpleo is a employment platform, geared to proffesionals who are not normally in others (like LinkedIn)',
-    },
-    {
-      title: 'Code Witchers',
-      src: codewitchers,
-      description:
-        'CodeWitchers is a software development company from Argentina',
-    },
-    {
-      title: 'Devaluarg',
-      // src: 'https://media.istockphoto.com/id/485112085/es/foto/d%C3%B3lares-en-primer-plano.jpg?s=612x612&w=0&k=20&c=-Ni4_ApeGd11_NjSSPJRdwpwGV6nBzoii0hh7sDrWt8=',
-      src: devaluarg,
-      description:
-        'DevaluARG is a website with information about the prices of the dollar, bitcoin and other cryptocurrencies.',
-    },
-    {
-      title: 'Cancheros',
-      src: cancheros,
-      description:
-        'Cancheros is a web application for fans of football. The user can save the times they went to the matches, their results and their favorite teams',
-    },
-  ];
-
   return (
-    <div style={{ backgroundColor: '#210011', textAlign: 'center' }}>
+    <div
+      style={{
+        backgroundColor: '#210011',
+        textAlign: 'center',
+        padding: '4rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '16px',
+      }}
+    >
       <TitleRemarcado contenido="PROJECTS" />
       <div
         style={{
@@ -70,7 +49,7 @@ export default function App() {
             }}
             //hover
             whileHover={{
-              scale: 1.1,
+              scale: 1.02,
               boxShadow: '0 0 10px rgba(0,0,0,0.4)',
             }}
             transition={{
@@ -90,22 +69,28 @@ export default function App() {
 
             {selectedWorkIndex === index && (
               <>
-                <motion.p
-                  style={{ width: '100%', margin: '16px' }}
+                <motion.div
+                  style={{
+                    width: '100%',
+                    margin: '16px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '16px',
+                    alignItems: 'center',
+                  }}
                   initial={{ opacity: 0 }}
                   animate={{
                     opacity: 1,
                   }}
                 >
-                  {/* <img src={work.src}></img> */}
-                  {work.description}
-                </motion.p>
-                <motion.a
-                  style={{ width: '100%', margin: '16px' }}
-                  whileHover={{ scale: 1.2 }}
-                >
-                  Ir a{' '}
-                </motion.a>
+                  <p>{work.description}</p>
+                  <p id="technologies">
+                    Technologies used: {work.technologies}
+                  </p>
+                  <a id="link" href={work.link}>
+                    View site
+                  </a>
+                </motion.div>
               </>
             )}
           </motion.div>
---
+++ src/components/worksArray.js
@@ -0,0 +1,38 @@
+import codewitchers from '../assets/images/codewitchers.svg';
+import cancheros from '../assets/images/cancheros.svg';
+import devaluarg from '../assets/images/devaluarg.svg';
+export const works = [
+  {
+    title: 'OnEmpleo',
+    src: 'https://onempleo.com/assets/logo_OnEmpleo.565f4192.svg',
+    description:
+      'OnEmpleo is a employment platform, geared to proffesionals who are not normally in others (like LinkedIn)',
+    link: 'https://onempleo.com',
+    technologies:
+      'React, NodeJS, Express, MongoDB, Firebase, styled-components',
+  },
+  {
+    title: 'Code Witchers',
+    src: codewitchers,
+    description:
+      'CodeWitchers is a software development company from Argentina',
+    link: 'https://codewitchers.com',
+    technologies: 'React, Node, styled-components',
+  },
+  {
+    title: 'Devaluarg',
+    src: devaluarg,
+    description:
+      'DevaluARG is a website with information about the prices of the dollar, bitcoin and other cryptocurrencies.',
+    link: 'https://devaluarg.web.app',
+    technologies: 'React, Node, APIRest',
+  },
+  {
+    title: 'Cancheros',
+    src: cancheros,
+    description:
+      'Cancheros is a web application for fans of football. The user can save the times they went to the matches, their results and their favorite teams',
+    link: 'https://cancheros-15f93.web.app/',
+    technologies: 'React, Node, Express, Mongo, Firebase for Hosting',
+  },
+];

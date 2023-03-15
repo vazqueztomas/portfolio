521537a3a183e8e5866af66f68122cf32d92392f
Tomas Vazquez <vazquezt2018@gmail.com>
2023-03-15 17:08:58 -0300

    new images

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,110 +1,116 @@
+import { motion } from 'framer-motion';
 import React, { useState } from 'react';
-import { motion, AnimateSharedLayout } from 'framer-motion';
 import TitleRemarcado from './TitleRemarcado';
+import codewitchers from '../assets/images/codewitchers.svg';
+import cancheros from '../assets/images/cancheros.svg';
+import devaluarg from '../assets/images/devaluarg.svg';
 import './Works.css';
 
-function Content({ day, disabled }) {
-  return (
-    <motion.div
-      className="title"
-      layoutId="title"
-      style={{ opacity: disabled ? 0.2 : 1 }}
-    >
-      <img src={day} />
-    </motion.div>
-  );
-}
-
-function ExpandedCard({ children, onCollapse }) {
-  return (
-    <>
-      <motion.div
-        className="card expanded"
-        layoutId="expandable-card"
-        onClick={onCollapse}
-      >
-        {children}
-      </motion.div>
-      {/* <motion.p
-        className="card expanded secondary"
-        onClick={onCollapse}
-        transition={{ delay: 0.3 }}
-        initial={{ opacity: 0, top: '6rem' }}
-        animate={{ opacity: 1, top: '3rem' }}
-      >
-        Today is clear
-      </motion.p> */}
-    </>
-  );
-}
-
-function CompactCard({ children, onExpand, disabled }) {
-  return (
-    <motion.div
-      className="card compact"
-      layoutId="expandable-card"
-      onClick={disabled ? undefined : onExpand}
-    >
-      {children}
-    </motion.div>
-  );
-}
-
-function DateButton({ day, onCollapse, onExpand, disabled }) {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const collapseDate = () => {
-    setIsExpanded(false);
-    onCollapse();
-  };
-
-  const expandDate = () => {
-    setIsExpanded(true);
-    onExpand();
-  };
-
-  return (
-    <div className="card-container">
-      <AnimateSharedLayout>
-        {isExpanded ? (
-          <ExpandedCard onCollapse={collapseDate} day={day}>
-            <Content day={day} disabled={disabled} />
-          </ExpandedCard>
-        ) : (
-          <CompactCard onExpand={expandDate} disabled={disabled} day={day}>
-            <Content day={day} disabled={disabled} />
-          </CompactCard>
-        )}
-      </AnimateSharedLayout>
-    </div>
-  );
-}
-
 export default function App() {
-  const [expandedDay, setCollapsedDay] = useState();
-  const days = [
-    'https://onempleo.com/assets/logo_OnEmpleo.565f4192.svg',
-    'https://img.freepik.com/vector-premium/diseno-logotipo-cara-mago-blanco-negro_497517-273.jpg?w=2000',
-    'https://media.istockphoto.com/id/485112085/es/foto/d%C3%B3lares-en-primer-plano.jpg?s=612x612&w=0&k=20&c=-Ni4_ApeGd11_NjSSPJRdwpwGV6nBzoii0hh7sDrWt8=',
-    'https://wallpapercave.com/wp/wp3164738.jpg',
+  const [showCard, setShowCard] = useState(false);
+  const [selectedWorkIndex, setSelectedWorkIndex] = useState(-1);
+
+  const works = [
+    {
+      title: 'OnEmpleo',
+      src: 'https://onempleo.com/assets/logo_OnEmpleo.565f4192.svg',
+      description:
+        'OnEmpleo is a employment platform, geared to proffesionals who are not normally in others (like LinkedIn)',
+    },
+    {
+      title: 'Code Witchers',
+      src: codewitchers,
+      description:
+        'CodeWitchers is a software development company from Argentina',
+    },
+    {
+      title: 'Devaluarg',
+      // src: 'https://media.istockphoto.com/id/485112085/es/foto/d%C3%B3lares-en-primer-plano.jpg?s=612x612&w=0&k=20&c=-Ni4_ApeGd11_NjSSPJRdwpwGV6nBzoii0hh7sDrWt8=',
+      src: devaluarg,
+      description:
+        'DevaluARG is a website with information about the prices of the dollar, bitcoin and other cryptocurrencies.',
+    },
+    {
+      title: 'Cancheros',
+      src: cancheros,
+      description:
+        'Cancheros is a web application for fans of football. The user can save the times they went to the matches, their results and their favorite teams',
+    },
   ];
 
   return (
-    <>
-      <div className="container">
-        <TitleRemarcado contenido="PROJECTS" />
-        <div className="dates">
-          {days.map(day => (
-            <DateButton
-              key={day}
-              day={day}
-              disabled={expandedDay !== day && expandedDay !== undefined}
-              onExpand={() => setCollapsedDay(day)}
-              onCollapse={() => setCollapsedDay()}
-            />
-          ))}
-        </div>
+    <div style={{ backgroundColor: '#210011', textAlign: 'center' }}>
+      <TitleRemarcado contenido="PROJECTS" />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          justifyContent: 'center',
+          paddingBottom: '10%',
+          background: '#210011',
+          gap: '8px',
+          margin: '0 10%',
+        }}
+      >
+        {works.map((work, index) => (
+          <motion.div
+            key={index}
+            style={{
+              background: '#A11B36',
+              padding: '16px 8px',
+              borderRadius: '8px',
+              boxShadow: '0 0 10px rgba(0,0,0,0.2)',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+              margin: '0 16px',
+            }}
+            //hover
+            whileHover={{
+              scale: 1.1,
+              boxShadow: '0 0 10px rgba(0,0,0,0.4)',
+            }}
+            transition={{
+              layout: {
+                duration: 1,
+                type: 'spring',
+              },
+            }}
+            layout
+            onClick={() => {
+              console.log(work.title);
+              setSelectedWorkIndex(index);
+              setShowCard(!showCard);
+            }}
+          >
+            <motion.img layout="position" key={work.src} src={work.src} />
+
+            {selectedWorkIndex === index && (
+              <>
+                <motion.p
+                  style={{ width: '100%', margin: '16px' }}
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 1,
+                  }}
+                >
+                  {/* <img src={work.src}></img> */}
+                  {work.description}
+                </motion.p>
+                <motion.a
+                  style={{ width: '100%', margin: '16px' }}
+                  whileHover={{ scale: 1.2 }}
+                >
+                  Ir a{' '}
+                </motion.a>
+              </>
+            )}
+          </motion.div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
